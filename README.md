d761ff41e717ec7407b7abfe7d8afb1e65a5fe12
danbaulk <danielbaulk@gmail.com>
2024-06-01 18:43:12 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FPL-Predictor
+
+This is the intermediary JAVA service which will be running the WEKA models, providing an interface for the backend Golang API to utilise its predictions.
+
+Using Spring Boot and Maven.
---
+++ src/test/java/com/fpl_predictor/fpl_predictor/FplPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpl_predictor.fpl_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
